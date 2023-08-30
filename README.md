51674c1d4921e8ea945ea66c7a6896944b3df5f6
nix <nix.loar@gmail.com>
2023-08-30 00:40:32 -0300

    basic gulp config

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # xpertize
-[<img alt="Deployed with FTP Deploy Action" src="https://img.shields.io/badge/Deployed With-FTP DEPLOY ACTION-%3CCOLOR%3E?style=for-the-badge&color=0077b6">](https://github.com/SamKirkland/FTP-Deploy-Action)
+
+Página de conversão feita para a Xpertize
+
+# Instalação
+
+Para rodar o projeto, comece instalando as dependências 
+
+### `npm install`
+
+Em seguida, para abrir o modo desenvolvimento digite
+
+### `npm start`
+
+# Estrutura
+
+- Para uma maior facilidade na manutenção, as sessões foram dívidas em arquivos HTML separados e concatenados com o gulp. Da mesma forma, as folhas de estilo e os scripts também foram dívidas por escopo
+
+- O bundle de produção é gerado pelo gulp na pasta **dist**. Os arquivos CSS e JS contidos lá estão minificados e _não devem ser alterados diretamente_
+
+- Pushes na branch `main` fazem com que os arquivos do bundle de produção vá automaticamente para o servidor de hospedagem via FTP [<img alt="Deployed with FTP Deploy Action" src="https://img.shields.io/badge/Deployed With-FTP DEPLOY ACTION-%3CCOLOR%3E?style=for-the-badge&color=0077b6">](https://github.com/SamKirkland/FTP-Deploy-Action)
+
--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,16 @@
 const { src, dest, series, watch } = require('gulp');
 const concat = require('gulp-concat');
+const browserSync = require('browser-sync').create();
+
+// html
+
+function html() {
+    return src(['./page-sections/header.html', './page-sections/footer.html'])
+        .pipe( concat('index.html'))
+        .pipe( dest ('./') )
+        .pipe( dest('./dist/') )
+        .pipe( browserSync.stream() )
+}
 
 // styles
 
@@ -11,7 +22,10 @@ function styles() {
         .pipe( concat('style.css'))
         .pipe( autoprefixer('last 2 versions') )
         .pipe( cssMinify() )
+        .pipe( dest('./') )
         .pipe( dest('./dist/') )
+        .pipe( browserSync.stream() )
+
 }
 
 // script
@@ -20,17 +34,27 @@ const jsMinify = require('gulp-terser');
 
 function scripts() {
     return src('./scripts/*.js')
+    .pipe( concat('bundle.js'))
     .pipe( jsMinify() )
+    .pipe( dest('./') )
     .pipe( dest('./dist/') )
+    .pipe( browserSync.stream() )
 }
 
 //Watchtask
 
 function watchTask(){
-    watch(
-        ['./styles/*.css', './scripts/*.js'],
-        series(styles, scripts)
-    )
+
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+
+    watch('./styles/*.css', styles)
+    watch('./scripts/*.js', scripts)
+    watch('./page-sections/*.html', html)
+    watch('index.html').on('change', browserSync.reload);
 }
 
-exports.default = series(styles, scripts, watchTask);
+exports.default = series(html, styles, scripts, watchTask);
